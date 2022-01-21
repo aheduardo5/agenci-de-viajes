9b764dd8d22302069b5e40f924f24679bc325463
Eduardo Aguilar <aheduardo5@gmail.com>
2022-01-21 17:59:35 -0600

    Project finished

--- js/script.js
+++ js/script.js
@@ -65,3 +65,25 @@ const swiper = new Swiper('.swiper', {
         },
     },
   });
+
+
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+  });
